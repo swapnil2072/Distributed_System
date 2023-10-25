4a4f67a807fdfa86ae8aa3da39e40bab76ba514e
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-25 19:02:41 +0530

    added menu to bully algo

--- BullyElection.java
+++ BullyElection.java
@@ -23,10 +23,44 @@ public class BullyElection {
             processes.add(new Process(i));
         }
 
-        System.out.print("Enter the process ID initiating the election: ");
-        int initiatingProcessID = scanner.nextInt();
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Kill a Process");
+            System.out.println("2. Revive a Process");
+            System.out.println("3. Start an Election");
+            System.out.println("4. Send a Message to Coordinator");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            int option = scanner.nextInt();
 
-        initiateElection(processes, initiatingProcessID);
+            switch (option) {
+                case 1:
+                    System.out.print("Enter the ID of the process to kill: ");
+                    int processToKill = scanner.nextInt();
+                    processes.get(processToKill - 1).active = false;
+                    System.out.println("Process " + processToKill + " has been killed.");
+                    break;
+                case 2:
+                    System.out.print("Enter the ID of the process to revive: ");
+                    int processToRevive = scanner.nextInt();
+                    processes.get(processToRevive - 1).active = true;
+                    System.out.println("Process " + processToRevive + " has been revived.");
+                    break;
+                case 3:
+                    System.out.print("Enter the process ID initiating the election: ");
+                    int initiatingProcessID = scanner.nextInt();
+                    initiateElection(processes, initiatingProcessID);
+                    break;
+                case 4:
+                    sendMessageToCoordinator(processes);
+                    break;
+                case 5:
+                    System.out.println("Exiting the program.");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please select a valid option.");
+            }
+        }
     }
 
     public static void initiateElection(ArrayList<Process> processes, int initiatingProcessID) {
@@ -65,5 +99,25 @@ public class BullyElection {
                 break;
             }
         }
+    }
+
+    public static void sendMessageToCoordinator(ArrayList<Process> processes) {
+        for (Process process : processes) {
+            if (process.active && process.processID == findCoordinator(processes)) {
+                System.out.println("Message sent to Coordinator (Process " + process.processID + ") " );
+                return;
+            }
+        }
+        System.out.println("No coordinator found to send the message.");
+    }
+
+    public static int findCoordinator(ArrayList<Process> processes) {
+        int highestProcessID = -1;
+        for (Process process : processes) {
+            if (process.active && process.processID > highestProcessID) {
+                highestProcessID = process.processID;
+            }
+        }
+        return highestProcessID;
     }
 }
